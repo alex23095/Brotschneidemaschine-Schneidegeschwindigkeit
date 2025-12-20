e1c2b1d4c0b187324fb8096e2aab59fa1f44adae
alex23095 <alexander.kueblbeck@stud.th-deg.de>
2025-12-20 17:11:21 +0100

    monitoringService.cpp

--- src/04_monitoringService/monitoringService.cpp
+++ src/04_monitoringService/monitoringService.cpp
@@ -1,25 +1,15 @@
 #include "monitoringService.hpp"
 
-MonitoringService::MonitoringService(CurrentSensor& sensor, std::uint32_t sampleIntervalMs)
-    : sensor_{ sensor }
-    , sampleIntervalMs_{ sampleIntervalMs }
-    , lastSampleTime_{ std::chrono::steady_clock::now() }
-    , lastCurrentMa_{ 0U }
-    , overcurrent_{ false }
+MonitoringService::MonitoringService()
+    : overCurrentLimitmA_(5000)   // Dummy: 5A Grenze
 {
 }
 
-bool MonitoringService::updateMonitoring()
-{
-    const auto now = std::chrono::steady_clock::now();
-    const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastSampleTime_).count();
-
-    if (static_cast<std::uint32_t>(elapsed) < sampleIntervalMs_) {
-        return false;
-    }
-
-    lastCurrentMa_ = sensor_.readCurrent();
-    overcurrent_ = sensor_.checkOvercurrent(lastCurrentMa_);
-    lastSampleTime_ = now;
-    return true;
+bool MonitoringService::checkOverCurrent(int currentmA) const {
+    return currentmA > overCurrentLimitmA_;
 }
+
+bool MonitoringService::updateMonitoring(int currentmA) const {
+    return checkOverCurrent(currentmA);
+}
+
